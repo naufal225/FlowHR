e8cf14be1244cb94e818b7b98c5f09f411f915fe
naufal225 <naufalmarufashrori225@gmail.com>
2026-03-24 22:53:08 +0700

    menambahkan mapper untuk exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,16 @@
 <?php
 
+use App\Exceptions\Attendance\AttendanceException;
 use App\Http\Middleware\CheckFeatureActive;
 use App\Http\Middleware\EnsureHasDivision;
 use App\Http\Middleware\RoleMiddleware;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Foundation\Configuration\Exceptions;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withSchedule(function (Schedule $schedule) {
@@ -28,6 +32,57 @@ return Application::configure(basePath: dirname(__DIR__))
             "feature" => CheckFeatureActive::class,
             "division" => EnsureHasDivision::class,
         ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+       // 1. HANDLE SEMUA AttendanceException (BASE)
+        $exceptions->render(function (
+            AttendanceException $e,
+            Request $request
+        ) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+                'code' => $e->getErrorCode(),
+            ], $e->getStatusCode());
+        });
+
+        // 2. VALIDATION ERROR (Form Request / Validator)
+        $exceptions->render(function (
+            ValidationException $e,
+            Request $request
+        ) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Data tidak valid.',
+                'code' => 'VALIDATION_ERROR',
+                'errors' => $e->errors(),
+            ], 422);
+        });
+
+        // 3. AUTH ERROR API MOBILE
+        $exceptions->render(function (
+            AuthenticationException $e,
+            Request $request
+        ) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthorized.',
+                'code' => 'UNAUTHORIZED',
+            ], 401);
+        });
+
+        // 4. FALLBACK (ERROR SYSTEM)
+        $exceptions->render(function (
+            Throwable $e,
+            Request $request
+        ) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Terjadi kesalahan pada server.',
+                'code' => 'INTERNAL_SERVER_ERROR',
+            ], 500);
+        });
+
     })
     ->withCommands([
         \App\Console\Commands\RunQueueOnce::class,
