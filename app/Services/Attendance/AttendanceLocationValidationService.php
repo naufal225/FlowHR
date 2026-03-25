bf987712ceb3e77c469944ae38416a5c838fba28
naufal225 <naufalmarufashrori225@gmail.com>
2026-03-25 19:34:59 +0700

    menambahkan latitude dan longitude ke AttendancePolicyData

---
+++ app/Services/Attendance/AttendanceLocationValidationService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Service\Attendance;
+
+class AttendanceLocationValidationService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
